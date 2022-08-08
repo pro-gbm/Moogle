1dd2a4a78b0c493216f69393aeb3b065a02327b0
Junghyun100 <qorwjdgus8951@gmail.com>
2022-08-08 21:22:57 +0900

    init

---
+++ Moogle/Moogle/src/main/java/com/progbm/Moogle/MoogleApplication.java
@@ -0,0 +1,13 @@
+package com.progbm.Moogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoogleApplication.class, args);
+	}
+
+}
---
+++ Moogle/Moogle/src/test/java/com/progbm/Moogle/MoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progbm.Moogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
