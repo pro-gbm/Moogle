39e3beef6c61b1ba5d97b2ff85544fbc8d495fe6
RGunny <sadog94@gmail.com>
2022-10-16 22:38:43 +0900

    [Fix #20] 500 중복 에러코드 삭제 및 기본 응답 호출 변경

--- backend/src/main/java/com/progbm/Moogle/exception/ErrorCode.java
+++ backend/src/main/java/com/progbm/Moogle/exception/ErrorCode.java
@@ -11,11 +11,11 @@ public enum ErrorCode {
     SUCCESS(HttpStatus.OK, "0000", "성공"),
 
     /* 기타 오류 처리 */
-    RUNTIME_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "500","런타임 오류"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500","서버 오류"),
+    RUNTIME_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "5001","런타임 오류"),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "401","권한 없음"),
     FORBIDDEN(HttpStatus.FORBIDDEN, "403","접근 거부됨"),
-    NULL_POINTER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "500", "NPE 발생")
+    NULL_POINTER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "5002", "NPE 발생")
     ;
 
 
