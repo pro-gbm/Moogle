142ded0c1bc96c348069ddf1488f2681686a85c2
me-hana <hayonleee@gmail.com>
2022-10-31 19:39:56 +0900

    [Design #25] Survey 메인 영역 비율 수정

--- frontend/src/components/survey/SurveyMain.jsx
+++ frontend/src/components/survey/SurveyMain.jsx
@@ -2,11 +2,22 @@ import Option from './Option';
 
 function SurveyMain() {
   return (
-    <div style={{ backgroundColor: 'yellowgreen', width: '80%' }}>
+    <div
+      style={{
+        backgroundColor: 'yellowgreen',
+        width: '80%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
       <div>질문지 영역</div>
-      <Option />
-      <Option />
-      <Option />
+      <div style={{ height: '80%', backgroundColor: 'aliceblue' }}>
+        <Option />
+        <Option />
+        <Option />
+      </div>
       <div>버튼 영역</div>
     </div>
   );
