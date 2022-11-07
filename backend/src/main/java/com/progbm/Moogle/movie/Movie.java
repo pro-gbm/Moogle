35d4bbea36e22d9402b162c734bf653e1d25cf24
meloncha <meloncha0205@gmail.com>
2022-11-07 22:15:40 +0900

    [Feat #43] Movie Entity 생성

---
+++ backend/src/main/java/com/progbm/Moogle/movie/Movie.java
@@ -0,0 +1,47 @@
+package com.progbm.Moogle.movie;
+
+import com.progbm.Moogle.director.Director;
+import com.progbm.Moogle.util.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Movie extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Integer tId;
+
+    private String title;
+
+    private LocalDateTime openingDate;
+
+    private Double score;
+
+    private Integer attendance;
+
+    private String thumbnailUrl;
+
+    // N : 1 관계
+//    private Director director;
+
+//    private Nation nation;
+
+    // N : N 관계
+//    private List<Actor> actorList;
+
+//    private List<Genre> genreList;
+
+//    private List<Ott> ottList;
+
+}
