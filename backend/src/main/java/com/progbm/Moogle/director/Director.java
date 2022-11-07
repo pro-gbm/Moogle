7c011d256760ae0f02110424cafb8de25819ef88
RGunny <sadog94@gmail.com>
2022-11-07 22:01:27 +0900

    [Feat #43] Director, Genre 어노테이션, tId 추가

--- backend/src/main/java/com/progbm/Moogle/director/Director.java
+++ backend/src/main/java/com/progbm/Moogle/director/Director.java
@@ -2,20 +2,22 @@ package com.progbm.Moogle.director;
 
 import com.progbm.Moogle.util.BaseTimeEntity;
 import com.progbm.Moogle.util.Gender;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Director extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    private Integer tId;
 
     private String name;
 
