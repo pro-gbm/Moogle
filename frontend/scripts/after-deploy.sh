30547873d5b332f3c8362a4d279f3237e88fd6dc
jslee <dlwltjd7778@naver.com>
2023-01-16 22:15:24 +0900

    [Feat #24] frontend test

--- frontend/scripts/after-deploy.sh
+++ frontend/scripts/after-deploy.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/app/frontend
+PROJECT_ROOT="/home/ubuntu/app/frontend"
+APP_LOG="$PROJECT_ROOT/log/application.log"
+ERROR_LOG="$PROJECT_ROOT/log/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/log/deploy.log"
 
-cd $REPOSITORY
+
+cd $PROJECT_ROOT
+
+TIME_NOW=$(date +%c)
 
 sudo yarn
 
+echo "$TIME_NOW > START" >> $DEPLOY_LOG
 sudo pm2 start dist
+echo "$TIME_NOW > END" >> $DEPLOY_LOG
+
+
+ENC_KEY=${ENC_KEY}
+
+echo "암호화키 >> $ENC_KEY" >> $DEPLOY_LOG
+
