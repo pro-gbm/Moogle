992fff6873f4b78049c95c864932688a09b3da2d
meloncha <meloncha0205@gmail.com>
2023-01-25 17:55:59 +0900

    [Feat #46] WebClient 를 이용한 TMDB API 호출 테스트

---
+++ backend/src/main/java/com/progbm/Moogle/movie/MovieRepository.java
@@ -0,0 +1,8 @@
+package com.progbm.Moogle.movie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
