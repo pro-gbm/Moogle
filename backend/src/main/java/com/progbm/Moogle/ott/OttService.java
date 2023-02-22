1aa25cea7ab78f554cc83125dce4b11f904abc93
Junghyun100 <qorwjdgus8951@gmail.com>
2023-02-22 21:22:00 +0900

    [Feat #46] ott  providerId 컬럼 값 추가 테스트 및 필터링

--- backend/src/main/java/com/progbm/Moogle/ott/OttService.java
+++ backend/src/main/java/com/progbm/Moogle/ott/OttService.java
@@ -1,5 +1,6 @@
 package com.progbm.Moogle.ott;
 
+import com.progbm.Moogle.movie.Movie;
 import com.progbm.Moogle.nation.Nation;
 import com.progbm.Moogle.nation.NationRepository;
 import com.progbm.Moogle.tmdb.TmdbService;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -23,14 +25,20 @@ public class OttService {
     private final TmdbService tmdbService;
     private final String IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
-    public List<Ott> Otts() {
+    public void saveOtts() {
+        Set<Integer> savedOttTmdbIds = ottRepository.findAll().stream().map(Ott::getProviderId).collect(Collectors.toSet());
+
         OttResponse ottResponses = tmdbService.getOtts();
+        // TODO : 필터 기능 추가
         List<Ott> ottList = ottResponses.getResults().stream().map(
-                ottResponse -> Ott.builder().ott((ottResponse.getProvider_name())).thumbnailUrl(IMAGE_URL + ottResponse.getLogo_path()).build())
+                ottResponse -> Ott.builder().ott((ottResponse.getProvider_name())).providerId(ottResponse.getProvider_id()).thumbnailUrl(IMAGE_URL + ottResponse.getLogo_path()).build())
                 .collect(Collectors.toList());
-        for ( var ott: ottList ) {
-            System.out.println("saveOtts() = " + ott.getOtt() + " " + ott.getThumbnailUrl());
-        }
-        return ottList;
+        ottList = ottList.stream().filter(ott -> !savedOttTmdbIds.contains(ott.getProviderId())).collect(Collectors.toList());
+        ottRepository.saveAll(ottList);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Ott> getOtts() {
+        return ottRepository.findAll();
     }
 }
