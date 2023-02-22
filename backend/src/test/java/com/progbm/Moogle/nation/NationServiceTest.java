f8525033f20b974882b9fd1c1ccb2fc14989f6d4
Junghyun100 <qorwjdgus8951@gmail.com>
2023-02-22 22:03:34 +0900

    [Feat #46] nation TestCode 추가

--- backend/src/test/java/com/progbm/Moogle/nation/NationServiceTest.java
+++ backend/src/test/java/com/progbm/Moogle/nation/NationServiceTest.java
@@ -2,6 +2,7 @@ package com.progbm.Moogle.nation;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,14 +11,19 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class NationServiceTest {
 
+    @Autowired
+    private NationService nationService;
+
     @Test
     @DisplayName("국가 삽입 & 조회 테스트")
     @Transactional
-    void insertNations() {
-
+    void insertNationsTest() {
+        nationService.insertNations();
     }
 
     @Test
-    void getNations() {
+    void getNationsTest() {
+        var list = nationService.getNations();
+        assertEquals(list.size() == 0, false);
     }
 }
