24b91c14eddab3d73141df21eba56619d2ef3821
me-hana <hayonleee@gmail.com>
2023-02-22 22:41:43 +0900

    [DESIGN #33] modify button component

--- frontend/src/components/common/Button.jsx
+++ frontend/src/components/common/Button.jsx
@@ -1,76 +1,82 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 /////////////// Type
 // color: default, disabled, warning
 // variant: outlined, filled
 // size: small, medium, large
 // value: any string
+// icon: icon component
+// iconPosition: front, back
 
 export default function Button(props) {
-  const { color, size, value, variant } = props;
+  const { color, size, value, variant, iconPosition } = props;
   const Icon = props.icon;
 
   const parseProps = () => {
-    let text_color = "";
-    let border_color = "";
-    let bg_color = "";
+    let text_color = '';
+    let border_color = '';
+    let bg_color = '';
     let padding = 0;
     let font_size = 0;
     let border_radius = 0;
 
     switch (size) {
-      case "small":
+      case 'small':
         padding = 5;
         font_size = 12;
         border_radius = 8;
         break;
-      case "medium":
+      case 'medium':
         padding = 8;
         font_size = 17;
         border_radius = 10;
         break;
-      case "large":
+      case 'large':
         padding = 10;
         font_size = 22;
         border_radius = 12;
         break;
+      default:
+        padding = 8;
+        font_size = 22;
+        border_radius = 12;
     }
 
     switch (variant) {
-      case "outlined":
-        if (color === "warning") {
-          text_color = "#E2202C";
-          border_color = "#E2202C";
-          bg_color = "#FFFFFF";
-        } else if (color === "disabled") {
-          text_color = "#D9D9D9";
-          border_color = "#D9D9D9";
-          bg_color = "#FFFFFF";
+      case 'outlined':
+        if (color === 'warning') {
+          text_color = '#E2202C';
+          border_color = '#E2202C';
+          bg_color = '#FFFFFF';
+        } else if (color === 'disabled') {
+          text_color = '#D9D9D9';
+          border_color = '#D9D9D9';
+          bg_color = '#FFFFFF';
         } else {
-          text_color = "#ffcb00";
-          border_color = "#ffcb00";
-          bg_color = "#FFFFFF";
+          text_color = '#ffcb00';
+          border_color = '#ffcb00';
+          bg_color = '#FFFFFF';
         }
         break;
-      case "filled":
-        if (color === "warning") {
-          text_color = "#FFFFFF";
-          border_color = "#E2202C";
-          bg_color = "#E2202C";
-        } else if (color === "disabled") {
-          text_color = "#FFFFFF";
-          border_color = "#D9D9D9";
-          bg_color = "#D9D9D9";
+      case 'filled':
+        if (color === 'warning') {
+          text_color = '#FFFFFF';
+          border_color = '#E2202C';
+          bg_color = '#E2202C';
+        } else if (color === 'disabled') {
+          text_color = '#FFFFFF';
+          border_color = '#D9D9D9';
+          bg_color = '#D9D9D9';
         } else {
-          text_color = "#FFFFFF";
-          border_color = "#ffcb00";
-          bg_color = "#ffcb00";
+          text_color = '#FFFFFF';
+          border_color = '#ffcb00';
+          bg_color = '#ffcb00';
         }
         break;
       default:
-        text_color = "#FFFFFF";
-        border_color = "#FFFFFF";
-        bg_color = "#FFFFFF";
+        text_color = '#FFFFFF';
+        border_color = '#FFFFFF';
+        bg_color = '#FFFFFF';
     }
 
     return {
@@ -84,12 +90,12 @@ export default function Button(props) {
   };
 
   const [feature, setFeature] = useState({
-    text_color: parseProps().text_color,
-    border_color: parseProps().border_color,
-    bg_color: parseProps().bg_color,
-    padding: parseProps().padding,
-    font_size: parseProps().font_size,
-    border_radius: parseProps().border_radius,
+    text_color: '#FFFFFF',
+    border_color: '#FFFFFF',
+    bg_color: '#FFFFFF',
+    padding: 8,
+    font_size: 22,
+    border_radius: 12,
   });
 
   useEffect(() => {
@@ -113,25 +119,29 @@ export default function Button(props) {
         paddingRight: `calc(${feature.padding}px + 10px)`,
         fontSize: `${feature.font_size}px`,
         borderRadius: `${feature.border_radius}px`,
-        width: "fit-content",
-        whiteSpace: "nowrap",
-        fontWeight: "bold",
-        cursor: color !== "disabled" ? "pointer" : "default",
-        display: "flex",
-        alignItems: "center",
-        "&:hover": {
-          boxShadow: color !== "disabled" ? "1px 2px 3px #DDD" : undefined,
-          transition: "box-shadow 0.2s ease-out",
+        width: 'fit-content',
+        whiteSpace: 'nowrap',
+        fontWeight: 'bold',
+        cursor: color !== 'disabled' ? 'pointer' : 'default',
+        display: 'flex',
+        alignItems: 'center',
+        '&:hover': {
+          boxShadow: color !== 'disabled' ? '1px 2px 3px #DDD' : undefined,
+          transition: 'box-shadow 0.2s ease-out',
         },
       }}
     >
-      {Icon && (
-        <Icon
-          fontSize="small"
-          sx={{ paddingRight: "5px", cursor: "inherit" }}
-        />
+      {iconPosition === 'front' && Icon && (
+        <div style={{ paddingRight: '5px' }}>
+          <Icon fontSize="small" sx={{ cursor: 'inherit' }} />
+        </div>
       )}
       {value.toUpperCase()}
+      {iconPosition === 'back' && Icon && (
+        <div style={{ paddingLeft: '5px' }}>
+          <Icon fontSize="small" sx={{ cursor: 'inherit' }} />
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/survey/SurveyMain.jsx
+++ frontend/src/components/survey/SurveyMain.jsx
@@ -1,5 +1,5 @@
-import Button from "../common/Button";
-import Option from "./Option";
+import Button from '../common/Button';
+import Option from './Option';
 
 function SurveyMain(props) {
   const { data } = props;
@@ -7,30 +7,52 @@ function SurveyMain(props) {
   return (
     <div
       style={{
-        width: "80%",
-        height: "100%",
-        marginTop: "50px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        color: "#FFF",
+        width: '80%',
+        height: '100%',
+        marginTop: '50px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        color: '#FFF',
       }}
     >
-      <div style={{ fontSize: "2rem", marginLeft: "15%", marginRight: "15%" }}>
+      <div style={{ fontSize: '2rem', marginLeft: '15%', marginRight: '15%' }}>
         {data.id}. {data.title}
       </div>
       <div
-        style={{ marginLeft: "15%", marginRight: "15%", marginBottom: "30px" }}
+        style={{ marginLeft: '15%', marginRight: '15%', marginBottom: '30px' }}
       >
         {data.description}
       </div>
-      <div style={{ minHeight: "75%", maxHeight: "80%" }}>
+      <div style={{ minHeight: '75%', maxHeight: '80%' }}>
         {data.options.map((option, index) => (
           <Option key={`${option.id}-${index}`} data={option} />
         ))}
       </div>
-      <div style={{ height: "10%", backgroundColor: "#FFF" }}>
-        <Button color="default" size="medium" value="Next" variant="outlined" />
+      <div
+        style={{
+          height: '7%',
+          marginLeft: '25%',
+          marginRight: '25%',
+          display: 'flex',
+          justifyContent: 'space-around',
+          // backgroundColor: '#FFF',
+        }}
+      >
+        <Button
+          color="default"
+          size="medium"
+          value="Prev"
+          variant="filled"
+          iconPosition="back"
+        />
+        <Button
+          color="default"
+          size="medium"
+          value="Next"
+          variant="filled"
+          iconPosition="back"
+        />
       </div>
     </div>
   );
