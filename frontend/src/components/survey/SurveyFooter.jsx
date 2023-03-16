1d307e59e944693bd2d264ec97535b5b4d38443d
me-hana <hayonleee@gmail.com>
2023-03-16 15:30:13 +0900

    [DESIGN #33] apply emotion for css

--- frontend/src/components/survey/SurveyFooter.jsx
+++ frontend/src/components/survey/SurveyFooter.jsx
@@ -1,19 +1,18 @@
-import { CONST } from '../../constants';
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import { CONST } from "../../constants";
+
+const footer = css({
+  height: CONST.FOOTERHEIGHT,
+  width: "100%",
+  color: "#FFF",
+  display: "flex",
+  alignItems: "center",
+});
 
 function SurveyFooter() {
-  return (
-    <div
-      style={{
-        height: CONST.FOOTERHEIGHT,
-        width: '100%',
-        color: '#FFF',
-        display: 'flex',
-        alignItems: 'center',
-      }}
-    >
-      여기는 푸터고 pro-gbm 로고가 들어갈거야
-    </div>
-  );
+  return <div css={footer}>여기는 푸터고 pro-gbm 로고가 들어갈거야</div>;
 }
 
 export default SurveyFooter;
--- frontend/src/components/survey/SurveyMain.jsx
+++ frontend/src/components/survey/SurveyMain.jsx
@@ -1,47 +1,61 @@
-import Button from '../common/Button';
-import Option from './Option';
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import Button from "../common/Button";
+import Option from "./Option";
+
+const surveyMain = css({
+  width: "80%",
+  height: "100%",
+  marginTop: "50px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  color: "#FFF",
+  ".title": {
+    fontSize: "2rem",
+    marginLeft: "15%",
+    marginRight: "15%",
+  },
+  ".description": {
+    marginLeft: "15%",
+    marginRight: "15%",
+    marginBottom: "30px",
+  },
+  ".options": {
+    minHeight: "75%",
+    maxHeight: "80%",
+  },
+});
+
+const buttonArea = css({
+  height: "7%",
+  marginLeft: "25%",
+  marginRight: "25%",
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center",
+  backgroundColor: "#FFF",
+});
 
 function SurveyMain(props) {
   const { data } = props;
 
   return (
-    <div
-      style={{
-        width: '80%',
-        height: '100%',
-        marginTop: '50px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        color: '#FFF',
-      }}
-    >
-      <div style={{ fontSize: '2rem', marginLeft: '15%', marginRight: '15%' }}>
+    <div css={surveyMain}>
+      <div className="title">
         {data.id}. {data.title}
       </div>
-      <div
-        style={{ marginLeft: '15%', marginRight: '15%', marginBottom: '30px' }}
-      >
-        {data.description}
-      </div>
-      <div style={{ minHeight: '75%', maxHeight: '80%' }}>
+      <div className="description">{data.description}</div>
+      <div className="options">
         {data.options.map((option, index) => (
           <Option key={`${option.id}-${index}`} data={option} />
         ))}
       </div>
-      <div
-        style={{
-          height: '7%',
-          marginLeft: '25%',
-          marginRight: '25%',
-          display: 'flex',
-          justifyContent: 'space-around',
-          // backgroundColor: '#FFF',
-        }}
-      >
+      <div css={buttonArea}>
         <Button
-          color="default"
-          size="medium"
+          color="warning"
+          size="large"
           value="Prev"
           variant="filled"
           iconPosition="back"
