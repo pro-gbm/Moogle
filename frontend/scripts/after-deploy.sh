242860cb2b8645da1f97b4e2226a0cb68a1df462
jslee <dlwltjd7778@naver.com>
2023-03-21 22:06:10 +0900

    [Feat #24] codedeploy 충돌 test

--- frontend/scripts/after-deploy.sh
+++ frontend/scripts/after-deploy.sh
@@ -3,6 +3,7 @@ PROJECT_ROOT="/home/ubuntu/app/frontend/build"
 APP_LOG="/home/ubuntu/app/frontend/log/application_front.log"
 ERROR_LOG="/home/ubuntu/app/log/error_front.log"
 DEPLOY_LOG="/home/ubuntu/app/log/deploy_front.log"
+#test
 
 
 cd $PROJECT_ROOT
