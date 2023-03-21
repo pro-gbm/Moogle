16c647d9315d050f29fb57e0b6d93fdab70994b1
jslee <dlwltjd7778@naver.com>
2023-03-21 22:17:41 +0900

    [Feat #24] npm start 추가

--- frontend/scripts/after-deploy.sh
+++ frontend/scripts/after-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROJECT_ROOT="/home/ubuntu/app/frontend/build"
+PROJECT_ROOT="/home/ubuntu/app/frontend"
 APP_LOG="/home/ubuntu/app/frontend/log/application_front.log"
 ERROR_LOG="/home/ubuntu/app/log/error_front.log"
 DEPLOY_LOG="/home/ubuntu/app/log/deploy_front.log"
@@ -10,10 +10,8 @@ cd $PROJECT_ROOT
 
 TIME_NOW=$(date +%c)
 
-sudo yarn
-
 echo "$TIME_NOW > START" >> $DEPLOY_LOG
-sudo pm2 start dist
+sudo npm start &
 echo "$TIME_NOW > END" >> $DEPLOY_LOG
 
 
