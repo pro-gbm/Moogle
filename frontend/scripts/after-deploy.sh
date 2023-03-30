a55c6adbeddfea003a7df7caccc2e1e2d10c7a5d
jslee <dlwltjd7778@naver.com>
2023-03-30 21:15:55 +0900

    [Feat #24] github action test 230330-1

--- frontend/scripts/after-deploy.sh
+++ frontend/scripts/after-deploy.sh
@@ -3,7 +3,7 @@ PROJECT_ROOT="/home/ubuntu/app/frontend"
 APP_LOG="/home/ubuntu/app/frontend/log/application_front.log"
 ERROR_LOG="/home/ubuntu/app/log/error_front.log"
 DEPLOY_LOG="/home/ubuntu/app/log/deploy_front.log"
-#test
+# test1212
 
 
 cd $PROJECT_ROOT
